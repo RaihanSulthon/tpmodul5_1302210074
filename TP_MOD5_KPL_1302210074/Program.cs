835767b6afc8921448b1124b23a51bc99fc79c1c
RaihanSulthon <raihansy@student.telkomuniversity.ac.id>
2023-03-19 16:39:02 +0700

    Menambahkan class HaloGeneric dengan method SapaUser

--- TP_MOD5_KPL_1302210074/Program.cs
+++ TP_MOD5_KPL_1302210074/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace TP_MOD5_KPL_1302210074
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric<String> sapa= new HaloGeneric<String>();
+
+            String X = "Raihan Sulthon";
+
+            sapa.SapaUSer(X);
+        }
+    }
+}
